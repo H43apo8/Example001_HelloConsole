473e0135844f90ed25ac9d4acb3625aabced05bc
H43apo8 <H43apo8@yandex.ru>
2022-11-30 13:51:10 +0300

    Add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string Username = Console.ReadLine();
+
+    if(Username.ToLower() == "красавчик")
+    {
+        Console.WriteLine("Еб@ть ты Красавчик!");
+    }
+    else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(Username);
+    }
