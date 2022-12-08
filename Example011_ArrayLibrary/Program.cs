cbcfd326cc41141cefd18e201fc93c5ad2c2c0d0
H43apo8 <H43apo8@yandex.ru>
2022-12-08 13:26:05 +0300

    Ex.12 Виды методов

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -31,6 +31,7 @@ int IndexOf(int[] collection, int find)
         if (collection[index] == find)
         {
             position - index;
+            break;
         }
         index++;
     }
---
+++ Example012_Void 1/Program.cs
@@ -0,0 +1,56 @@
+﻿// Вид 1
+
+using System.Data;
+
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+
+Method1();
+
+//Вид 2
+void Method2(string message)
+{
+    Console.WriteLine(message);
+}
+Method2(message: "Текст сообщения");
+
+void Method2(string message, int count)
+{
+    int i = 0;
+    while (i<count)
+    {
+        Console.WriteLine(message);
+        i++;
+    }
+}
+Method2(count: 4, message: "новый текст");
+
+//ВИД 3 
+
+int Method3();
+{
+    return DataTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+//Вид 4 
+
+string Method4(int count, char c)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "qwerrty");
+Console.WriteLine(res);
